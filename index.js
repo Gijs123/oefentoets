cf91de2050812679dda28aefce55e52b5d2f4139
Gijs123 <33117276+Gijs123@users.noreply.github.com>
2019-02-12 09:56:11 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -9,12 +9,28 @@ function kiesBoot(){
   verwijderKeuze.remove(verwijderKeuze.selectedIndex);
 
   let kiesBoot = new Boottocht(tochtnummer, vertrekTijd, "", "", bootNummer);
-  console.log(bootNummer);
+  array.push(kiesBoot)
+//  console.log(array);
   document.getElementById('dataID').innerHTML = JSON.stringify(kiesBoot);
 }
 
 function klantKomtTerug(){
   let tochtnummer = document.getElementById("tochtnummerIDTerug").value;
   let eindTijd = document.getElementById("eindTijdID").value;
-  console.log(eindTijd);
+//  console.log(eindTijd);
+  for (let i=0; i<array.length; i++){
+    if (array[i].tochtnummer === tochtnummer){
+  //    console.log(array[i].tochtnummer);
+    //  console.log(array[i].beginTijd);
+    //  console.log(array[i][vertrekTijd]);
+    let duurVanTocht = eindTijd - array[i].beginTijd;
+    console.log(duurVanTocht);
+    console.log(array[i].beginTijd);
+    }
+  }
+
+//  let duurVanTocht = eindTijd - array.vertrekTijd;
+  //console.log(duurVanTocht);
 }
+
+let array = [];
