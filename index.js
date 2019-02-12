f2dca89138b5551b18ad96e85910101c3707fce5
Gijs123 <33117276+Gijs123@users.noreply.github.com>
2019-02-12 12:29:08 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,35 +2,62 @@
 
 function kiesBoot(){
   let bootNummer = document.getElementById("bootNummerID").value;
-  let vertrekTijd = document.getElementById("vertrekTijdID").value;
+  let beginTijd = document.getElementById("vertrekTijdID").value;
   let tochtnummer = document.getElementById("tochtnummerID").value;
-
   let verwijderKeuze = document.getElementById("bootNummerID");
   verwijderKeuze.remove(verwijderKeuze.selectedIndex);
-
-  let kiesBoot = new Boottocht(tochtnummer, vertrekTijd, "", "", bootNummer);
+  let kiesBoot = new Boottocht(tochtnummer, beginTijd, "", "", bootNummer);
   array.push(kiesBoot)
-//  console.log(array);
   document.getElementById('dataID').innerHTML = JSON.stringify(kiesBoot);
 }
 
 function klantKomtTerug(){
   let tochtnummer = document.getElementById("tochtnummerIDTerug").value;
   let eindTijd = document.getElementById("eindTijdID").value;
-//  console.log(eindTijd);
   for (let i=0; i<array.length; i++){
     if (array[i].tochtnummer === tochtnummer){
-  //    console.log(array[i].tochtnummer);
-    //  console.log(array[i].beginTijd);
-    //  console.log(array[i][vertrekTijd]);
     let duurVanTocht = eindTijd - array[i].beginTijd;
-    console.log(duurVanTocht);
-    console.log(array[i].beginTijd);
+
+    let EndTIme = eindTijd;
+    let StartTime = array[i].beginTijd;
+    var minutes = parseTime(EndTIme) - parseTime(StartTime);
+    // console.log(minutes);
+    document.getElementById('dataID2').innerHTML = "minuten verstreken " + minutes;
+    document.getElementById('startMin').innerHTML = "minuten onderweg " + minutes;
+    return minutes
     }
   }
-
-//  let duurVanTocht = eindTijd - array.vertrekTijd;
-  //console.log(duurVanTocht);
 }
 
 let array = [];
+let minu = [];
+
+function parseTime(s) {
+   var c = s.split(':');
+   return parseInt(c[0]) * 60 + parseInt(c[1]);
+}
+
+function prijsBerekening(minutes){
+  let weer = document.getElementById("weerTypeID").value;
+  let seizoen = document.getElementById("seizoenID").value;
+  let drukte = document.getElementById("drukteID").value;
+  console.log(weer);
+  console.log(seizoen);
+  console.log(drukte);
+  let min = minutes;
+  console.log(typeof min);
+}
+
+// function parseTime(s) {
+//    var c = s.split(':');
+//    return parseInt(c[0]) * 60 + parseInt(c[1]);
+// }
+//   let EndTIme = "14:44";
+//   let StartTime = "14:00";
+//   var minutes = parseTime(EndTIme) - parseTime(StartTime);
+//   console.log(minutes);
+// let c = a - b;
+// console.log(c);
+
+// let d = new Date(11:13:00);
+// console.log(d);
